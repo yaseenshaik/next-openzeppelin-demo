6a98e2077059b50a0f58ded540b80b2e86e51c73
Yaseen Shaik <17909518+yaseenshaik@users.noreply.github.com>
2024-01-13 14:36:43 -0500

    feat: add shadcn and index page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
+import { ThemeProvider } from "@/components/theme-provider"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Main />
+          <NextScript />
+        </ThemeProvider>
       </body>
     </Html>
   )
